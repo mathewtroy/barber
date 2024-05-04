a454a153bbd0f521545a94a4d6a3f3188dd05f93
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-05 01:35:57 +0200

    add Team page

---
+++ team.php
@@ -0,0 +1,49 @@
+<?php
+   session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Team</title>
+
+      <!-- Import CSS section from PHP file -->
+      <?php require "inc/_style.php" ?>
+
+   </head>
+
+   <body>
+
+      <!-- Import header section from PHP file -->
+      <?php require "inc/_header.php" ?>
+
+      <div class="main">
+         <!-- background of about -->
+         <div class="heading-team"> 
+            
+            
+         
+         </div>
+      </div>
+
+      <section>
+
+         <div>
+            Team
+         </div>
+
+
+
+
+      </section>
+
+
+      <!-- Import footer section from PHP file -->
+      <?php require "inc/_footer.php" ?>
+
+      <!-- images from website: https://wallpapers.com/ -->
+   </body>
+</html>

119f34cdf8b322d3f6e156f196e90c1d0e93947d
Kross Aleksandr <krossale@fel.cvut.cz>
2024-05-06 00:21:24 +0200

    upgrade Price page

--- price.php
+++ price.php
@@ -29,12 +29,77 @@
         </div>
       </div>
 
-      <section>
+      <section class="price">
 
-         <div>
-            Content
+      <h2 class="heading-title">Best price in Prague</h2>
+
+         <div class="box-container">
+
+            <div class="box">
+               <div class="image">
+                  <img src="./static/img/hair/classic.png">
+               </div>
+               <div class="content">
+                  <h3>Classic haircut</h3>
+                  <h4>500 CZK</h4>
+                  
+                  <p>
+                     A timeless haircut including trimming, hair washing, contour shaping with 
+                     clippers and razor, and grooming of ear hair.
+                  </p>
+                  <a href="book.php" class="btn">Book now</a>
+               </div>
+            </div>
+
+            <div class="box">
+               <div class="image">
+                  <img src="./static/img/hair/standart.png">
+               </div>
+               <div class="content">
+                  <h3>Standart haircut</h3>
+                  <h4>700 CZK</h4>
+
+                  <p>
+                     A comprehensive service featuring a haircut from scratch, hair washing, 
+                     contour shaping with clippers and razor, and ear hair trimming.
+                  </p>
+                  <a href="book.php" class="btn">Book now</a>
+               </div>
+            </div>
+
+            <div class="box">
+               <div class="image">
+                  <img src="./static/img/hair/beard.png">
+               </div>
+               <div class="content">
+                  <h3>Beard </h3>
+                  <h4>500 CZK</h4>
+
+                  <p>
+                     Expert grooming for your beard, including contour shaping with clippers and razor, 
+                     hot towel steam, face massage, and ear hair grooming.
+                  </p>
+                  <a href="book.php" class="btn">Book now</a>
+               </div>
+            </div>
+
+            <div class="box">
+               <div class="image">
+                  <img src="./static/img/hair/premium.png">
+               </div>
+               <div class="content">
+                  <h3>Premium haircut</h3>
+                  <h4>1000 CZK</h4>
+
+                  <p>
+                     Our top-tier service offering a complete haircut from scratch, hair washing, beard grooming, 
+                     contour shaping with clippers and razor, hot towel steam, face massage, ear hair grooming, and beard balm application.
+                  </p>
+                  <a href="book.php" class="btn">Book now</a>
+               </div>
+            </div>
+
          </div>
-
 
 
 
